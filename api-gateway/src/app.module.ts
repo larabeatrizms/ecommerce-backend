40b414cce05ee0b41f42d3bc58f74759a474919a
Lara Beatriz <31808723+larabeatrizms@users.noreply.github.com>
2022-09-10 21:49:57 -0300

    feat(api-gateway): documentation and env config

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,16 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+
+import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ClientsModule.register([
       {
         name: 'CUSTOMER_SERVICE',
         transport: Transport.TCP,
         options: {
-          host: '127.0.0.1',
+          host: 'localhost',
           port: 8888,
         },
       },
--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,13 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const logger = new Logger();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(3000);
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('E-commerce API')
+    .setDescription('The e-commerce API documentation.')
+    .setVersion('1.0.0')
+    .addTag('Customer')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT);
 
   logger.log('API Gateway is listening', 'Initialization');
 }
